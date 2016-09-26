48d534b44adad64023130d4d02e2d5ea99089c9f
Pichchan9k <pichchanok.n@hotmail.com>
2016-09-26 14:23:16 +0800

    user finale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { User } from './user'
 
+
+const USERS = [
+    new User("jfk", "abc", 20, 50, 170),
+    new User("jjj", "abc", 30, 50, 170),
+    new User("222", "abc", 29, 50, 170)
+] 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,15 +15,47 @@ import { User } from './user'
 })
 export class AppComponent {
   title:string = "this is my title";
-  user:User
+  selectedUser:User
+  users:Array<User>
+  isEditMode:Boolean = false;
 
   constructor(){
-    this.user = new User();
-    this.user.name = "Josh";
-    this.user.age = 30
-    this.user.weight = 50;
-    this.user.height = 70;
-
+    this.selectedUser = USERS[0];
+    this.users = USERS;
   }
 
+  onCheckAge(){
+    //if(this.user.age >= 100)
+    if(this.selectedUser.isOld())
+      alert("Too Old")
+    else
+      alert("Ok")
+  }
+
+  onCheckBMI(){
+    alert("BMI:" + this.selectedUser.getBMI() )
+  }
+
+  deleteUser(index){
+    this.users.splice(index, 1);
+  }
+
+  addUser(){
+    let tempUser:any = {};
+    tempUser.name = prompt("name")
+    tempUser.password = prompt("password")
+    tempUser.age = Number(prompt("age"))
+    tempUser.weight = Number(prompt("weight"))
+    tempUser.height = Number(prompt("height"))
+    this.users.push( new User(
+      tempUser.name,
+      tempUser.password,
+      tempUser.age,
+      tempUser.weight,
+      tempUser.height
+    )  )//end of this.users.push
+  }//end of addUser
+
+
 }
+
